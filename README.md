96bb7276fa4e736f29c7c7f2d474884313a3eaa6
Juan931 <juandavidds08@gmail.com>
2021-08-23 10:59:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-c1ykc7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-c1ykc7)
---
+++ index.ts
@@ -0,0 +1,42 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+//Contiene por lo menos el comportamiento de presentarse a sí mismo(introduce())Protagonista principal que hereda de Character No olvides agregar algunos comportamientos propios de este personaje. (métodos)
+
+abstract class Character {
+  nameAntagonist: string ="Jhon Martin"
+  nameProtagonist: string ="Juan Casas ";
+  introduced(){
+  return "My name is " + this.nameProtagonist; 
+  }
+  introduced1(){
+    return "My name is"+ this.nameAntagonist;
+  }
+ 
+}
+
+//implementar metodo en protagonista principal
+class ProtagonistaP extends Character{
+    protagonist(){
+        return "Y yo soy el protagonista,y soy el heroe "
+      }
+    }
+
+const protagonist= new ProtagonistaP();
+console.log(protagonist.introduced())
+console.log(protagonist.protagonist())
+
+//Antagonista principal que hereda de Character. No olvides agregar algunos comportamientos propios de este personaje. (métodos)
+
+class antagonistaP extends Character{
+  antagonist(){
+    return "Yo soy el antagonista, y soy el malo."
+  }
+}
+const antagonist= new antagonistaP();
+console.log(antagonist.introduced1())
+console.log(antagonist.antagonist());
